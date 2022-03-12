c80c97a970a6f12653dfaf751cb789c01c3a76d9
saneenKP <saneenkp2000@gmail.com>
2022-03-12 22:28:14 +0530

    initialized using vscode

---
+++ simeple-rest-api/src/main/java/com/saneen/simeplerestapi/SimepleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.saneen.simeplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimepleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimepleRestApiApplication.class, args);
+	}
+
+}
