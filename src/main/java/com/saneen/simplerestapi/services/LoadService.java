910f97df8ab3e0a0f71167d3c26ed98d3025594f
saneenKP <saneenkp2000@gmail.com>
2022-03-14 23:55:38 +0530

    services added

---
+++ src/main/java/com/saneen/simplerestapi/services/LoadService.java
@@ -0,0 +1,11 @@
+package com.saneen.simplerestapi.services;
+
+import java.util.List;
+
+import com.saneen.simplerestapi.entities.LoadDetails;
+
+public interface LoadService {
+
+    public List<LoadDetails> getLoadDetails();
+    
+}
