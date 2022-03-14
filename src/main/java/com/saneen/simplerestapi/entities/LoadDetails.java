716c5a908fe8423ce9c29667f4a04be200827d35
saneenKP <saneenkp2000@gmail.com>
2022-03-14 22:33:56 +0530

    Generated getters and setters for the entity

---
+++ src/main/java/com/saneen/simplerestapi/entities/LoadDetails.java
@@ -0,0 +1,36 @@
+package com.saneen.simplerestapi.entities;
+
+public class LoadDetails {
+
+    private long id;
+    private String title;
+    private String description;
+
+    LoadDetails(){}
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+
+}
