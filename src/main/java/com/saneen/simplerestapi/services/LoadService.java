f18bc56d81d67825cc5d1f903d2f904fe5eb5651
saneenKP <saneenkp2000@gmail.com>
2022-03-15 18:33:11 +0530

    get load based on shipper id completed

--- src/main/java/com/saneen/simplerestapi/services/LoadService.java
+++ src/main/java/com/saneen/simplerestapi/services/LoadService.java
@@ -1,12 +1,13 @@
 package com.saneen.simplerestapi.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.saneen.simplerestapi.entities.LoadDetails;
 
 public interface LoadService {
 
     public List<LoadDetails> getLoadDetails();
-    public List<LoadDetails> getLoad();
+    public List<LoadDetails> getLoad(UUID shipperID);
     
 }
