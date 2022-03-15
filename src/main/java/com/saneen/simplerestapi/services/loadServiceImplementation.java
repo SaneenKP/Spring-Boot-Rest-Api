9785c0daf964edf736a6b297a5e9dfb3ce993f0a
saneenKP <saneenkp2000@gmail.com>
2022-03-15 17:57:38 +0530

    load service implementation constructor created according to the api data

--- src/main/java/com/saneen/simplerestapi/services/loadServiceImplementation.java
+++ src/main/java/com/saneen/simplerestapi/services/loadServiceImplementation.java
@@ -1,5 +1,33 @@
 package com.saneen.simplerestapi.services;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.saneen.simplerestapi.entities.LoadDetails;
+
+import org.apache.tomcat.jni.Local;
+
 public class loadServiceImplementation implements LoadService{
+
+    
+    List<LoadDetails> list;
+
+    public loadServiceImplementation() {
+
+        list = new ArrayList<>();
+        list.add(new LoadDetails("delhi", "jaipur", "chemicals", "canter", 1, 100, "this is just a comment", UUID.randomUUID(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-mm-yyyy")))
+    }
+
+
+
+    @Override
+    public List<LoadDetails> getLoadDetails() {
+
+        return null;
+    }
     
 }
