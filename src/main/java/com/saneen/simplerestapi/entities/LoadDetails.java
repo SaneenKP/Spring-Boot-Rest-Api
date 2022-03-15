a9a0be5a6bc92d961df7446095206ac01ec0b26c
saneenKP <saneenkp2000@gmail.com>
2022-03-15 17:38:17 +0530

    entity clas load details updated according to api data

--- src/main/java/com/saneen/simplerestapi/entities/LoadDetails.java
+++ src/main/java/com/saneen/simplerestapi/entities/LoadDetails.java
@@ -1,36 +1,89 @@
 package com.saneen.simplerestapi.entities;
 
+import java.sql.Date;
+
 public class LoadDetails {
 
-    private long id;
-    private String title;
-    private String description;
+    private String loadingPoint;
+    private String unloadingPoint;
+    private String productType;
+    private String truckType;
+    private int noOfTrucks;
+    private int weight;
+    private String comment;
+    private String shipperID;
+    private String date;
 
-    LoadDetails(){}
-
-    public long getId() {
-        return this.id;
+    public String getLoadingPoint() {
+        return this.loadingPoint;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setLoadingPoint(String loadingPoint) {
+        this.loadingPoint = loadingPoint;
     }
 
-    public String getTitle() {
-        return this.title;
+    public String getUnloadingPoint() {
+        return this.unloadingPoint;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setUnloadingPoint(String unloadingPoint) {
+        this.unloadingPoint = unloadingPoint;
     }
 
-    public String getDescription() {
-        return this.description;
+    public String getProductType() {
+        return this.productType;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setProductType(String productType) {
+        this.productType = productType;
     }
-    
+
+    public String getTruckType() {
+        return this.truckType;
+    }
+
+    public void setTruckType(String truckType) {
+        this.truckType = truckType;
+    }
+
+    public int getNoOfTrucks() {
+        return this.noOfTrucks;
+    }
+
+    public void setNoOfTrucks(int noOfTrucks) {
+        this.noOfTrucks = noOfTrucks;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getShipperID() {
+        return this.shipperID;
+    }
+
+    public void setShipperID(String shipperID) {
+        this.shipperID = shipperID;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
 }
