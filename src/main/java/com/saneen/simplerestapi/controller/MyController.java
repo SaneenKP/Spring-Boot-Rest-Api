e9681fc7a6af83284570e3cf6229f373aa3716be
saneenKP <saneenkp2000@gmail.com>
2022-03-15 18:03:52 +0530

    auto wired annotation completed

--- src/main/java/com/saneen/simplerestapi/controller/MyController.java
+++ src/main/java/com/saneen/simplerestapi/controller/MyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.saneen.simplerestapi.entities.LoadDetails;
 import com.saneen.simplerestapi.services.LoadService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+    @Autowired
     private LoadService loadService;
+
     @GetMapping("/load")
     public List<LoadDetails> getLoadDetails(){
         return loadService.getLoadDetails();
